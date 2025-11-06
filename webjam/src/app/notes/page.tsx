f5b5808526add4c876b8ce3874d01a349b0c6c8a
Evelyn Jiang <jianger@uci.edu>
2025-11-05 19:37:25 -0800

    Adjusted nav bar styles

--- webjam/src/app/notes/page.tsx
+++ webjam/src/app/notes/page.tsx
@@ -4,17 +4,22 @@ import StickyNote from "./stickynote";
 export default function NotesPage() {
   return (
     <>
-      <nav className="flex items-center justify-end font-[var(--font-sans)] p-4 text-xl">
+      <nav className="font-[var(--font-sans)] text-xl" style={{ position: 'absolute', right: '0px', top: '0px', display: 'inline-flex', padding: '15px 130.61px 2px 173.44px', justifyContent: 'flex-end', alignItems: 'center' }}>
         {/* Simple navigation links, need to make it look nice */}
-        <ul className="flex space-x-4">
+        <ul className="flex space-x-8">
           <li>
-            <Link href="/home">Home</Link>
+            <Link href="/home" style={{ color: '#353D48', textAlign: 'center', fontFamily: 'Nunito', fontSize: '19px', fontStyle: 'normal', fontWeight: 400, lineHeight: '19px' }}>Home</Link>
+          </li>
+          <li style={{ position: 'relative' }}>
+            <div style={{ position: 'absolute', top: '-55px', left: '50%', transform: 'translateX(-50%)', width: '100px', height: '45px', flexShrink: 0, borderRadius: '30px', background: '#F8E1BF' }}></div>
+            <Link href="/notes" style={{ color: '#353D48', textAlign: 'center', fontFamily: 'Nunito', fontSize: '19px', fontStyle: 'normal', fontWeight: 400, lineHeight: '19px' }}>Notes</Link>
           </li>
           <li>
-            <Link href="/notes">Notes Page</Link>
+            <Link href="/learn" style={{ color: '#353D48', textAlign: 'center', fontFamily: 'Nunito', fontSize: '19px', fontStyle: 'normal', fontWeight: 400, lineHeight: '19px' }}>Learn</Link>
           </li>
         </ul>
       </nav>
+      
       <div className="flex p-8 ">
         <h1 className="text-3xl font-semibold">Your Notes of Learning</h1>
       </div>
