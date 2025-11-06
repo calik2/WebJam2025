169f437de308efa811f3dc4a62a899c7ee10c00f
Sophia Zhang <sophiazhang321@gmail.com>
2025-11-06 14:37:44 -0800

    tags api routing hopefully

--- webjam/src/pages/api/tags/add_tag.ts
+++ webjam/src/pages/api/tags/add_tag.ts
@@ -1,18 +1,18 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import createClient from '@/utils/supabase/api'
 
-export default async function add_entry(req: NextApiRequest, res: NextApiResponse) {
+export default async function add_tag(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
         res.status(405).appendHeader('Allow', 'POST').end()
         return
     }
 
     const supabase = createClient(req, res)
-    const { uid, title, description } = req.body
+    const { name, uid } = req.body
 
     const { data, error } = await supabase
-        .from('entries')
-        .insert([{ uid, title, description }])
+        .from('tags')
+        .insert([{ name, uid }])
         .select('*')
 
     if (error) {
