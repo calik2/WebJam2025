f62d70a07d982cfcf3dc6c1d86abadbdfd696149
Evelyn Jiang <jianger@uci.edu>
2025-11-05 18:34:19 -0800

    Left-aligned the text!

--- webjam/src/app/home/texteditor.tsx
+++ webjam/src/app/home/texteditor.tsx
@@ -36,7 +36,7 @@ export default function PaperTiptap() {
     editorProps: {
       attributes: {
         class:
-          "ProseMirror justify-center items-center outline-none text-4xl font-[var(--font-lora)] !font-[var(--font-lora)] leading-relaxed p-10 ",
+          "ProseMirror justify-left items-left outline-none text-4xl font-[var(--font-lora)] !font-[var(--font-lora)] leading-relaxed p-10 ",
         style: `         
           color: #77777B;
           font-family: Lora;
@@ -44,6 +44,7 @@ export default function PaperTiptap() {
           font-style: normal;
           font-weight: 400;
           line-height: 51px;
+          text-align: left;
         `,
       },
     },
@@ -90,7 +91,7 @@ export default function PaperTiptap() {
 
   return (
     <>
-      <div className="flex justify-center items-center bg-[#f5f2e9] p-8 max-w-screen">
+      <div className="flex justify-left items-left bg-[#f5f2e9] p-8 max-w-screen">
         <EditorContent editor={editor} />
       </div>
       <div className="flex justify-center space-x-4 p-4 text-xl">
